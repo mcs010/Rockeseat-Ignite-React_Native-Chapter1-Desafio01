68b8a6410250fba75dc372fedc302ce6cb027071
mcs010 <math.cds10@gmail.com>
2021-06-01 21:43:41 -0300

    Update in Home, TodoInput and MyTasksList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 
 import { Header } from '../components/Header';
 import { MyTasksList } from '../components/MyTasksList';
@@ -11,18 +12,52 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+    //TODO - add new task if it's not empty    
+
+    if (newTaskTitle === '') {
+      Alert.alert('Digite um título válido! \u{1F9D0}');
+      return
+    }
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...oldState, data]); //Adds the task to the list without altering old values
   }
 
   function handleMarkTaskAsDone(id: number) {
     //TODO - mark task as done if exists
+
+    // Searches for the task that matches the id(parameter)
+    const task = tasks.filter(item => item.id === id)[0];
+
+    task.done = !task.done;
+
+    setTasks(oldState => [...oldState]);
   }
+
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+/*
+    let oldTasks = tasks;
+
+    const taskToDelete = tasks.findIndex(item => {
+      return item.id === id;
+    });
+
+    let updatedTasks = oldTasks.splice(taskToDelete, 1);
+
+    setTasks(updatedTasks);*/
+
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id // Shows only the tasks that don't have the same id as the parameter
+    ));
   }
 
   return (
@@ -31,10 +66,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <MyTasksList 
-        tasks={tasks} 
-        onPress={handleMarkTaskAsDone} 
-        onLongPress={handleRemoveTask} 
+      <MyTasksList
+        tasks={tasks}
+        onPress={handleMarkTaskAsDone}
+        onLongPress={handleRemoveTask}
       />
     </>
   )
